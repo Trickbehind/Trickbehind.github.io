50bcf090b8f919555bc46e91d8f87e7d3f6ba5a2
SSPrafulTondomker <iit2016082@iiita.ac.in>
2018-06-11 01:55:53 +0530

    comparison backend prepared

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var mysql = require ('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: 'holla123',
+	password: 'Ts@9849493088',
 	database: 'Version2'
 });
 
+var l1 = [{name: "xxxx"}], l2 = [{name: "xxxx"}];
 connection.connect (function (error){
 	if (error){
 		console.log ("Error in Database");
@@ -39,8 +40,43 @@ app.get ("/Sign", function(req, res){
 });
 
 app.get ("/comparison", function(req, res){
-	res.render ("comparison");
+	res.render ("comparison", {l1: l1, l2: l2});
 });
+
+
+
+app.post ("/comparison", function(req, res){
+	var c1 = req.body.college1, c2 = req.body.college2;
+	var data = "select * from college where name = ? ";
+	var r1= [], r2 = [];
+	connection.query(data, [c1], function(err, result, fields){
+		if (err){
+			console.log("error in data base college 1");
+		}else{
+				r1 = result;
+				connection.query(data, [c2], function(err, result, fields){
+						if (err){
+							console.log("error in data base college 2");
+					}else{
+							r2 = result;
+							console.log (r1[0].name);
+							console.log (r2);
+							res.render ("comparison", {l1: r1, l2: r2});
+					}
+				});
+			}
+	});
+	
+
+	
+});
+
+
+
+
+
+
+
 app.listen (3000, function (){
 	console.log ("Server Started!!!");
 });
